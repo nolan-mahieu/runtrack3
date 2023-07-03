deb24067aac6b4670ea1214161afa0aa6320f1fd
nolan.mahieu@laplateforme.io <nolan.mahieu@laplateforme.io>
2023-07-03 10:59:01 +0200

    premier

--- jour01/job01/index.php
+++ jour01/job01/index.php
@@ -1,8 +1,15 @@
 <!DOCTYPE html>
-<html>
-    <body>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>index js job01</title>
         <script>
             alert( 'Hello Javascript !' );
         </script>
-    </body>
+    </head>
+<body>
+
+</body>
 </html>
